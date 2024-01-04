b7c7389e964f4c93919b33312e99dab0cbd80a4b
Szabina Korbai <skorbai@c1r5p7.hive.fi>
2024-01-04 14:23:29 +0200

    Changed to using exit() instead of just return

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 12:35:59 by skorbai           #+#    #+#             */
-/*   Updated: 2024/01/03 11:01:14 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/01/04 14:19:35 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int32_t	main(int argc, char **argv)
 	if (argc != 2)
 		ft_error("Error\nProgram must have exactly 1 argument, the map\n");
 	if (validate_map(argv[1]) == 1)
-		return (EXIT_FAILURE);
+		exit (EXIT_FAILURE);
 	data = init_window(argv[1]);
 	draw_map(data);
 	mlx_key_hook(data->window, ft_key_hook, data);
 	mlx_loop(data->window);
 	mlx_terminate(data->window);
 	free(data);
-	return (EXIT_SUCCESS);
+	exit (EXIT_SUCCESS);
 }
