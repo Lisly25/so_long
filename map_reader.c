540342949c2987a7c1523c5195e5f02b9d3043be
Szabina Korbai <skorbai@c1r5p7.hive.fi>
2024-01-04 14:29:31 +0200

    Map is now properly closed even in case of read error

--- map_reader.c
+++ map_reader.c
@@ -6,13 +6,21 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 12:17:35 by skorbai           #+#    #+#             */
-/*   Updated: 2024/01/03 10:42:25 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/01/04 14:28:08 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "so_long.h"
 #include "vector.h"
+
+static void	ft_error_and_close(const char *str, int fd, t_vector *map)
+{
+	perror(str);
+	free_map(map);
+	close(fd);
+	exit (EXIT_FAILURE);
+}
 
 t_vector	*read_map(char *map_path)
 {
@@ -28,11 +36,7 @@ t_vector	*read_map(char *map_path)
 	fd = open(map_path, O_RDONLY);
 	map_line = get_next_line(fd);
 	if (map_line == NULL)
-	{
-		ft_error_message("Error\nFailed to read map file\n");
-		free_map(map_vector);
-		exit (EXIT_FAILURE);
-	}
+		ft_error_and_close("Error\nFailed to read map file\n", fd, map_vector);
 	vector_add_back(map_vector, map_line);
 	while (map_vector->map[i++] != NULL)
 	{
