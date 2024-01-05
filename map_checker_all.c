b70c9b43a8277cfc1b434df976b1f98c009efd5f
Szabina Korbai <skorbai@c1r5p7.hive.fi>
2024-01-05 15:23:26 +0200

    Now only maps with .ber extension are parsed

--- map_checker_all.c
+++ map_checker_all.c
@@ -6,11 +6,22 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 15:38:43 by skorbai           #+#    #+#             */
-/*   Updated: 2024/01/05 14:18:38 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/01/05 15:20:55 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static int	check_file_name(char *map_path)
+{
+	size_t	len;
+
+	len = ft_strlen(map_path);
+	map_path = map_path + (len - 4);
+	if (ft_strncmp(map_path, ".ber", 4) != 0)
+		return (1);
+	return (0);
+}
 
 static int	check_map(t_vector *map)
 {
@@ -32,6 +43,11 @@ int	validate_map(char *map_path)
 	t_vector	*map;
 	int			result;
 
+	if (check_file_name(map_path) == 1)
+	{
+		ft_error_message("Error\nMap file must have .ber extension\n");
+		exit (1);
+	}
 	map = read_map(map_path);
 	if (map->map[0] == NULL)
 		ft_error("Error\nMap could not be read");
